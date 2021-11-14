407b46b9d310e866d2b525684925264104dbf277
Hijiahmed <hiji.ahmed.rae.js@tuwaiq.edu.sa>
2021-11-14 11:40:48 +0300

    s

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import Home from "./componants/Home";
+function App() {
+  return (
+    <>
+      <Home />
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/componants/Home.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+export default function Home() {
+  const [movies, setmovies] = useState([]);
+  useEffect(async () => {
+    // here is the change
+    const result = await axios.get("http://localhost:5000/movies");
+    console.log(result.data);
+    setmovies(result.data);
+  }, []);
+  return (
+    <div>
+      <h1>Welcome to Web Movies</h1>
+      {movies.map((elem, i) => {
+        return (
+          <div key={i}>
+            <p>{elem.name}</p>
+            <p>{elem.date}</p>
+            <p>{elem.description}</p>
+            <img src={elem.url} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
